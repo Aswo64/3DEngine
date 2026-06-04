ae05454a0d40feef1b0cce6ad5726017501eb42d
Aswo <aswinpandeya12@gmail.com>
2026-06-04 10:49:02 -0400

    final commit prolly
    formatted selection

--- engine.js
+++ engine.js
@@ -49,13 +49,13 @@ let cullBool = true;
 let dirLightBool = false;
 let brightness = 0;
 let lightDir = [];
-let colour = [0,100,0];
+let colour = [0, 100, 0];
 let viewNormal = false;
 let cw = true;
 let rotAnim = 0;
 let rotX = 0;
 let rotY = 0;
-let fov = Math.PI/1.7;
+let fov = Math.PI / 1.7;
 
 
 
@@ -68,7 +68,7 @@ fovSlider.addEventListener('input', () => {
 speedSlider.addEventListener('input', () => {
     speedScale = Number(speedSlider.value);
     speedValue.textContent = `Speed: ${Number(speedSlider.value).toFixed(2)}x`;
-    speed = Math.sqrt(biggest**2 + smallest**2)*speedScale;
+    speed = Math.sqrt(biggest ** 2 + smallest ** 2) * speedScale;
 });
 
 wireframeToggle.addEventListener('change', () => {
@@ -81,11 +81,11 @@ transparencyToggle.addEventListener('change', () => {
 
 fillObjToggle.addEventListener('change', () => {
     fillObj = fillObjToggle.checked;
-        if (transparencyContainer) {
-            transparencyContainer.style.visibility = fillObj ? 'visible' : 'hidden';
-            transparencyContainer.style.opacity = fillObj ? '1' : '0';
-            transparencyContainer.style.pointerEvents = fillObj ? 'auto' : 'none';
-        }
+    if (transparencyContainer) {
+        transparencyContainer.style.visibility = fillObj ? 'visible' : 'hidden';
+        transparencyContainer.style.opacity = fillObj ? '1' : '0';
+        transparencyContainer.style.pointerEvents = fillObj ? 'auto' : 'none';
+    }
     if (!fillObj) {
         transparencyToggle.checked = false;
         transparency = false;
@@ -251,14 +251,14 @@ function parseObjFile(fileText) {
             vs.push({ x, y, z });
         }
         if (line.startsWith('f ')) {
-                const parts = line.split(/\s+/);
-                // We subtract one because obj files start from 1 for faces, not 0
-                const a = parseInt(parts[1].split('/')[0], 10)-1;
-                const b = parseInt(parts[2].split('/')[0], 10)-1;
-                const c = parseInt(parts[3].split('/')[0], 10)-1;
+            const parts = line.split(/\s+/);
+            // We subtract one because obj files start from 1 for faces, not 0
+            const a = parseInt(parts[1].split('/')[0], 10) - 1;
+            const b = parseInt(parts[2].split('/')[0], 10) - 1;
+            const c = parseInt(parts[3].split('/')[0], 10) - 1;
 
-                fcs.push([a,b,c]);
-            }
+            fcs.push([a, b, c]);
+        }
     }
     const avgX = (minX + maxX) / 2;
     const avgY = (minY + maxY) / 2;
@@ -267,14 +267,14 @@ function parseObjFile(fileText) {
     // Resets the camera position so u don't have to go on a pilgramige to find the object
     dx = -avgX;
     dy = -avgY;
-    speed = Math.sqrt(biggest**2 + smallest**2)*speedScale;
-    dz = -avgZ+speed;
-    
+    speed = Math.sqrt(biggest ** 2 + smallest ** 2) * speedScale;
+    dz = -avgZ + speed;
+
 }
 
 
 
-reader.onload = function(e) {
+reader.onload = function (e) {
     const textContent = e.target.result;
     parseObjFile(textContent);
 };
@@ -299,36 +299,36 @@ dropzone.addEventListener('dragleave', (e) => {
     img.style.filter = 'invert(0%)';
 });
 
-function assignFile(file){
-    if(!file){
+function assignFile(file) {
+    if (!file) {
         console.log("nofile")
     }
 
     const extension = file.name.split('.').pop().toLowerCase();
     console.log("Extension:", extension);
-    
+
     if (extension === "obj") {
-      reader.readAsText(file);
+        reader.readAsText(file);
     }
-    else{
+    else {
         console.log("give valid file")
     }
-    
+
 }
 
 dropzone.addEventListener('drop', (e) => {
     e.preventDefault();
     dropzone.classList.remove('active')
     img.style.filter = '';
-    const {files} = e.dataTransfer;
+    const { files } = e.dataTransfer;
     assignFile(files[0])
 });
 
 
-document.getElementById('objFile').addEventListener('change', function(event) {
+document.getElementById('objFile').addEventListener('change', function (event) {
     const file = event.target.files[0];
     assignFile(file)
-    
+
 });
 
 
@@ -338,7 +338,7 @@ function clear() {
 }
 
 function point({ x, y }, z) {
-    const s = 20/(z*5)
+    const s = 20 / (z * 5)
     ctx.fillStyle = fg
     ctx.fillRect(x - s / 2, y - s / 2, s, s)
 }
@@ -356,12 +356,10 @@ function screen(p) {
     }
 }
 
-
-
 function project({ x, y, z }) {
     return {
-        y: y / (z * Math.tan(fov/2)),
-        x: (x * canvas.height/canvas.width) / (z * Math.tan(fov/2))
+        y: y / (z * Math.tan(fov / 2)),
+        x: (x * canvas.height / canvas.width) / (z * Math.tan(fov / 2))
     }
 }
 
@@ -415,38 +413,38 @@ function rotate_x({ x, y, z }, a) {
     }
 }
 
-function addVector(one, two){
+function addVector(one, two) {
     return {
         x: one.x + two.x,
         y: one.y + two.y,
-        z : one.z + two.z
+        z: one.z + two.z
     }
 }
 
-function subVector(one, two){
+function subVector(one, two) {
     return {
         x: one.x - two.x,
         y: one.y - two.y,
-        z : one.z - two.z
+        z: one.z - two.z
     }
 }
 
-function scalarVector(vector, scalar){
-    return{
+function scalarVector(vector, scalar) {
+    return {
         x: vector.x * scalar,
         y: vector.y * scalar,
         z: vector.z * scalar
     }
 }
 
-function crossProduct(p1, p2, p3){
+function crossProduct(p1, p2, p3) {
     let v1 = []
     let v2 = []
-    if(cw){
+    if (cw) {
         v1 = subVector(p2, p1)
         v2 = subVector(p3, p1)
     }
-    else{
+    else {
         v1 = subVector(p3, p1)
         v2 = subVector(p2, p1)
     }
@@ -454,23 +452,23 @@ function crossProduct(p1, p2, p3){
         x: v1.y * v2.z - v1.z * v2.y,
         y: v1.z * v2.x - v1.x * v2.z,
         z: v1.x * v2.y - v1.y * v2.x
-    }   
-}
-
-function dotProduct(v1, v2){
-    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-}
-
-function normVect(v1){
-    const t = Math.sqrt(v1.x**2 + v1.y**2 + v1.z**2)
-    return {
-        x: v1.x/t,
-        y: v1.y/t,
-        z: v1.z/t
     }
 }
 
-function midpoint(p1, p2, p3){
+function dotProduct(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+}
+
+function normVect(v1) {
+    const t = Math.sqrt(v1.x ** 2 + v1.y ** 2 + v1.z ** 2)
+    return {
+        x: v1.x / t,
+        y: v1.y / t,
+        z: v1.z / t
+    }
+}
+
+function midpoint(p1, p2, p3) {
     return {
         x: (p1.x + p2.x + p3.x) / 3,
         y: (p1.y + p2.y + p3.y) / 3,
@@ -478,118 +476,118 @@ function midpoint(p1, p2, p3){
     }
 }
 
-function newVert(v1){
-    return rotate_x(rotate_y(translate_y(translate_x(translate_z(rotate_x(rotate_y(v1, angle + rotY), angle+rotX), dz), dx), dy), mouseX*dt), -mouseY*dt)
+function newVert(v1) {
+    return rotate_x(rotate_y(translate_y(translate_x(translate_z(rotate_x(rotate_y(v1, angle + rotY), angle + rotX), dz), dx), dy), mouseX * dt), -mouseY * dt)
 }
 
 function frame() {
     clear()
-    if (keys['w']){ 
-        if(keys['shift']){
+    if (keys['w']) {
+        if (keys['shift']) {
             dy -= speed * dt
-        } else{
+        } else {
             dz -= Math.cos(yaw) * speed * dt
             dx -= Math.sin(yaw) * speed * dt
         }
     }
-    if (keys['s']){ 
-        if(keys['shift']){
+    if (keys['s']) {
+        if (keys['shift']) {
             dy += speed * dt
-        } else{
-            dz += Math.cos(yaw)* speed * dt
-            dx += Math.sin(yaw)* speed * dt
+        } else {
+            dz += Math.cos(yaw) * speed * dt
+            dx += Math.sin(yaw) * speed * dt
         }
     }
-    if (keys['a']){ 
+    if (keys['a']) {
         dx += Math.cos(yaw) * speed * dt
         dz -= Math.sin(yaw) * speed * dt
     }
-    if (keys['d']){ 
+    if (keys['d']) {
         dx -= Math.cos(yaw) * speed * dt
         dz += Math.sin(yaw) * speed * dt
     }
-    angle += Math.PI * dt * rotAnim /4
-    
-    
+    angle += Math.PI * dt * rotAnim / 4
+
+
     const sortedFaces = [...fcs].sort((a, b) => {
         const aAverageZ = a.reduce((sum, index) => sum + newVert(vs[index]).z, 0) / a.length
         const bAverageZ = b.reduce((sum, index) => sum + newVert(vs[index]).z, 0) / b.length
         return bAverageZ - aAverageZ
     })
-    
+
     // Each face is to be a triangle
     for (const fc of sortedFaces) {
         //Cross product uses assumption of clockwise rotation of vertice assignment
         const preTransMidpoint = midpoint(vs[fc[0]], vs[fc[1]], vs[fc[2]])
         const mp = newVert(preTransMidpoint)
         const n1 = newVert(addVector(normVect(crossProduct(vs[fc[1]], vs[fc[2]], vs[fc[0]])), preTransMidpoint))
-        
+
         const normal = normVect(subVector(n1, mp))
-        const toCam = normVect(subVector(mp, {x:0,y:0,z:0}))
+        const toCam = normVect(subVector(mp, { x: 0, y: 0, z: 0 }))
         const faceDir = dotProduct(normal, toCam)
-        
-        if(faceDir > 0 && cullBool){
+
+        if (faceDir > 0 && cullBool) {
             continue
         }
-        
+
         //We do a for loop that takes the fc element length bcs some obj files do not only have triangles, some have quads and n-gons, so the code above me will only work if fc has at least 3 components (a triangle, can be anything more complex than a triangle though as long as it lies on one plane)
         ctx.beginPath()
         for (let i = 0; i < fc.length; i++) {
             //Research how the rotation matrix works and why mouseY has to be negative
-            yaw = mouseX*dt
+            yaw = mouseX * dt
             const v1 = newVert(vs[fc[i]])
             const v2 = newVert(vs[fc[(i + 1) % fc.length]])
-            
-            if(v2.z <= 0 && v1.z <= 0){
+
+            if (v2.z <= 0 && v1.z <= 0) {
                 continue
             }
             if (v1.z <= 0 || v2.z <= 0) {
                 let p1 = v1
                 let p2 = v2
-                
+
                 if (v2.z <= 0) {
                     p1 = v2
                     p2 = v1
                 }
                 const t = (0.001 - p1.z) / (p2.z - p1.z)
                 const v3 = addVector(p1, scalarVector(subVector(p2, p1), t))
-                
+
                 ctx.moveTo(screen(project((p2))).x, screen(project((p2))).y)
                 line(screen(project((v3))))
             }
-            else{
+            else {
                 ctx.lineTo(screen(project(v1)).x, screen(project(v1)).y)
                 line(screen(project((v2))))
             }
         }
         ctx.closePath()
-        if(wireFramebool){
+        if (wireFramebool) {
             ctx.stroke()
         }
-        if(fillObj){
+        if (fillObj) {
             let shade = 0
-            if(dirLightBool){
+            if (dirLightBool) {
                 // I can't just use the camera as 0,0,0 because I need the light source to move with all objects, if I used the camera's origin, it would result in values that changes when I as the camera/player move, we don't want that, we want a vector that remains in the same direction that can be referred to when 1. calculating shading and 2. the player moves/rotates
-                for (const lv of lightDir){
-                    shade+=Math.min(1, Math.max(0, -dotProduct(normal, normVect(subVector(rotate_x(rotate_y(translate_y(translate_x(translate_z(lv, dz), dx), dy), mouseX*dt), -mouseY*dt),newVert({x:0, y:0, z:0}))))))
+                for (const lv of lightDir) {
+                    shade += Math.min(1, Math.max(0, -dotProduct(normal, normVect(subVector(rotate_x(rotate_y(translate_y(translate_x(translate_z(lv, dz), dx), dy), mouseX * dt), -mouseY * dt), newVert({ x: 0, y: 0, z: 0 }))))))
                 }
             }
-            else{
+            else {
                 shade = Math.min(1, Math.max(0, -faceDir))
             }
-            const alphaShade= Math.round(255*shade)*(1+(brightness/100))
-            if(transparency){
-                ctx.fillStyle = `rgba(${colour[0]}, ${colour[1]}, ${colour[2]}, ${shade*(1+(brightness/100))})`
+            const alphaShade = Math.round(255 * shade) * (1 + (brightness / 100))
+            if (transparency) {
+                ctx.fillStyle = `rgba(${colour[0]}, ${colour[1]}, ${colour[2]}, ${shade * (1 + (brightness / 100))})`
             }
-            else{
-                ctx.fillStyle = `rgb(${alphaShade+colour[0]}, ${alphaShade+colour[1]}, ${alphaShade+colour[2]})`
+            else {
+                ctx.fillStyle = `rgb(${alphaShade + colour[0]}, ${alphaShade + colour[1]}, ${alphaShade + colour[2]})`
             }
             ctx.fill()
         }
-        if(n1.z >= 0 && mp.z >= 0 && viewNormal == true){
+        if (n1.z >= 0 && mp.z >= 0 && viewNormal == true) {
             ctx.beginPath()
             ctx.moveTo(screen(project(n1)).x, screen(project(n1)).y);
-            line(screen(project(mp)))  
+            line(screen(project(mp)))
             point(screen(project(n1)), n1.z)
             ctx.closePath()
             ctx.stroke()
@@ -602,9 +600,9 @@ const keys = {}
 document.addEventListener('keydown', e => { keys[e.key.toLowerCase()] = true })
 document.addEventListener('keyup', e => { keys[e.key.toLowerCase()] = false })
 
-function mouseMovement(event){
-    mouseX += event.movementX/5;
-    mouseY += event.movementY/5;
+function mouseMovement(event) {
+    mouseX += event.movementX / 5;
+    mouseY += event.movementY / 5;
 }
 
 document.addEventListener('pointerlockchange', () => {
